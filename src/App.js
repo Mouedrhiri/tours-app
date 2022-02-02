b1db20acb3dfc649427bb98a5996024db99d56cc
Mohammed Ouedrhiri <mouedrhiri492@gmail.com>
2022-02-02 16:54:07 +0100

    Making Tours API Fetching APP

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTours = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refrech Again
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTours} />
+      <div className="title">
+        <button className="btn" onClick={() => setTours([])}>
+          Remove All
+        </button>
+        <h2
+          style={{
+            marginTop: "40px",
+          }}
+        >
+          By Mohammed Ouedrhiri
+        </h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
